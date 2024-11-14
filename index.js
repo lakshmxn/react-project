b9cbef917437067614fc56a4b11fcd4eb70303c2
lakshman <lakshman12b@gmail.com>
2024-11-14 17:19:30 +0530

    feat: initial step for react revision

---
+++ index.js
@@ -0,0 +1,3 @@
+const element = React.createElement('div',{},[React.createElement('h1',{},'Hey'),React.createElement('h1',{},'Hey 2')]);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(element);
